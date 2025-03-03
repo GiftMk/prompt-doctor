6d6c2faa8330bfd6a249cef14364b3a4c71dedfd
Gift Mkwara <gift.mkwara@gmail.com>
2025-03-03 19:02:29 +1300

    improved prompt for fetching feedback

--- src/components/actions/fetchFeedback.ts
+++ src/components/actions/fetchFeedback.ts
@@ -1,25 +1,27 @@
-'use server'
+"use server";
 
-import 'dotenv/config'
-import { promptGuidelines } from '@/lib/promptGuidelines'
-import { type Feedback, feedbackSchema } from '@/state/atoms'
-import { ChatOpenAI } from '@langchain/openai'
+import "dotenv/config";
+import { promptGuidelines } from "@/lib/promptGuidelines";
+import { type Feedback, feedbackSchema } from "@/state/atoms";
+import { ChatOpenAI } from "@langchain/openai";
 
 const model = new ChatOpenAI({
-	model: 'gpt-4o',
-	temperature: 0,
-})
+  model: "gpt-4o",
+  temperature: 0,
+});
 
 export const fetchFeedback = async (prompt: string): Promise<Feedback> => {
-	const structuredModel = model.withStructuredOutput(feedbackSchema)
-	const feedback = await structuredModel.invoke(`
+  const structuredModel = model.withStructuredOutput(feedbackSchema);
+  const feedback = await structuredModel.invoke(`
   ### Task ###
   - Improve and give feedback on the following prompt which is going to be sent to an LLM
   so that it is as effective as possible and meets the prompt guidelines from  OpenAI.
   
+  - If the prompt is good enough considering the guidelines, or is so simple it needs no further improvement. Leave the original prompt as is.
+
   - Keep the improved prompt simple and concise.
 
-  - Feedback should end on a complete sentence, avoid ending with colons
+  - feedback.response should end on a complete sentence, avoid ending with phrases like "here's the improved prompt:"
 
   ### OpenAI Prompt Guidelines ###
   ${JSON.stringify(promptGuidelines, null, 2)}
@@ -30,7 +32,7 @@ export const fetchFeedback = async (prompt: string): Promise<Feedback> => {
   ${prompt}
 
   ---
-  `)
+  `);
 
-	return feedback
-}
+  return feedback;
+};
