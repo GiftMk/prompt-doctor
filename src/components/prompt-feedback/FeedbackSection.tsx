e2d1d08d0edbc0b041da65169a3547261d49ee0a
Gift Mkwara <gift.mkwara@gmail.com>
2025-03-08 19:41:51 +1300

    removed framer motion imports

--- src/components/prompt-feedback/FeedbackSection.tsx
+++ src/components/prompt-feedback/FeedbackSection.tsx
@@ -1,42 +1,33 @@
-'use client'
+"use client";
 
-import { Markdown } from '@/components/Markdown'
-import { CopyButton } from '../CopyButton'
-import { cn } from '@/lib/utils'
-import { AnimatePresence, motion } from 'framer-motion'
+import { Markdown } from "@/components/Markdown";
+import { CopyButton } from "../CopyButton";
+import { cn } from "@/lib/utils";
 
 type FeedbackSectionProps = {
-	feedback?: string
-	title: string
-	disableCopy?: boolean
-	className?: string
-}
+  feedback?: string;
+  title: string;
+  disableCopy?: boolean;
+  className?: string;
+};
 
 export const FeedbackSection = ({
-	feedback,
-	title,
-	disableCopy,
-	className,
+  feedback,
+  title,
+  disableCopy,
+  className,
 }: FeedbackSectionProps) => {
-	const hideCopy = feedback?.length === 0 || disableCopy
+  const hideCopy = feedback?.length === 0 || disableCopy;
 
-	return (
-		<div
-			className={cn('flex w-fit flex-col overflow-auto px-8 pb-4', className)}
-		>
-			<span className='sticky top-0 flex items-center gap-2 bg-background'>
-				<p className='font-medium text-foreground/70 text-xs'>{title}</p>
-				<CopyButton text={feedback} className={cn({ invisible: hideCopy })} />
-			</span>
-			<AnimatePresence>
-				<motion.div
-					initial={{ opacity: 0, x: 10 }}
-					animate={{ opacity: 1, x: 0 }}
-					exit={{ opacity: 0, x: 10 }}
-				>
-					<Markdown>{feedback}</Markdown>
-				</motion.div>
-			</AnimatePresence>
-		</div>
-	)
-}
+  return (
+    <div
+      className={cn("flex w-fit flex-col overflow-auto px-8 pb-4", className)}
+    >
+      <span className="sticky top-0 flex items-center gap-2 bg-background">
+        <p className="font-medium text-foreground/70 text-xs">{title}</p>
+        <CopyButton text={feedback} className={cn({ invisible: hideCopy })} />
+      </span>
+      <Markdown>{feedback}</Markdown>
+    </div>
+  );
+};
